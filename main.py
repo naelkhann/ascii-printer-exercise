49ac540193592a0ef41e7c33a1ba43373b2cc58f
Nael Khan <naelkhann@gmail.com>
2023-01-24 02:47:17 -0500

    Update each coord to use layers

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+
 class AsciiPrinter:
     def __init__(self):
         # Each coord in canvas is a stack (can have multiple layers)
@@ -23,11 +24,22 @@ class AsciiPrinter:
 
     # Draw rect at x1,y1 (top left) to x2,y2 (bottom right)
     def drawRect(self, args, layer=None):
-        print("args", args)
+        def rankByLayer(item):
+            if item not in self.layers:
+                return 0
+            return self.layers.index(item)
+
         fill_char, left_x, top_y, right_x, bottom_y = args
+
         for i in range(int(top_y), int(bottom_y) + 1):
             for j in range(int(left_x), int(right_x) + 1):
+                fill_char_layer_idx = None
+                if fill_char in self.layers:
+                    fill_char_layer_idx = self.layers.index(fill_char)
+                # Append layer fill_char
                 self.canvas[i][j].append(fill_char)
+                # Sort layers
+                self.canvas[i][j].sort(key=rankByLayer)
 
         self.layers.append(fill_char)
         self.rectangles[fill_char] = {
@@ -43,20 +55,49 @@ class AsciiPrinter:
         left_x, top_y, right_x, bottom_y = args
         for i in range(int(top_y), int(bottom_y) + 1):
             for j in range(int(left_x), int(right_x) + 1):
-                self.canvas[i][j] = "-"
+                self.canvas[i][j] = []
 
     # Drag and drop from a select coordinate
     # Grabs rect at coordinate, and moves rect to new coordinate maintaining original shape
     def dragAndDrop(self, args):
+        def removeRect(fill_char, i, j):
+            if fill_char not in self.canvas[i][j]:
+                return
+
+            idx = self.canvas[i][j].index(fill_char)
+            self.canvas[i][j].pop(idx)
+
+            removeRect(fill_char, i + 1, j)
+            removeRect(fill_char, i - 1, j)
+            removeRect(fill_char, i, j + 1)
+            removeRect(fill_char, i, j - 1)
+
+        def calcEdges(fill_char, select_x, select_y, release_x, release_y):
+            top_y = self.rectangles[fill_char]["top_y"]
+            bottom_y = self.rectangles[fill_char]["bottom_y"]
+            left_x = self.rectangles[fill_char]["left_x"]
+            right_x = self.rectangles[fill_char]["right_x"]
+            deltaTop = select_y - int(top_y)
+            deltaBottom = int(bottom_y) - select_y
+            deltaLeft = select_x - int(left_x)
+            deltaRight = int(right_x) - select_x
+            newTop = release_y - deltaTop
+            newBottom = release_y + deltaBottom
+            newLeft = release_x - deltaLeft
+            newRight = release_x + deltaRight
+            removeRect(fill_char, select_y, select_x)
+            self.drawRect([fill_char, newLeft, newTop, newRight, newBottom])
+
         select_x, select_y, release_x, release_y = args
         select_x = int(select_x)
         select_y = int(select_y)
         release_x = int(release_x)
         release_y = int(release_y)
-        coord_has_rect = self.canvas[select_y][select_x] != "-"
+        # Get top most layer at coord
+        coord_has_rect = self.canvas[select_y][select_x][0]
         if coord_has_rect:
-            fill_char = self.canvas[select_y][select_x]
-            self.canvas[release_y][release_x] = fill_char
+            fill_char = self.canvas[select_y][select_x][0]
+            calcEdges(fill_char, select_x, select_y, release_x, release_y)
 
     # Parses line instruction, grabbing command and using rest of line as args for command
     def parseCommand(self, command, args):
